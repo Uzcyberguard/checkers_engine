de684a98f0affa92ae409bb7eeaec65bcf152821
Uzcyberguard <hasannormamatov696@gmail.com>
2026-06-24 18:22:27 +0500

    changed

--- evaluate_funksion.py
+++ evaluate_funksion.py
@@ -7,24 +7,33 @@ class Evaluate():
 
 
     def centralization(self, board):
-        for i in board:
-            for j in i:
-                if board[i][j] != 0:
-                    pass
+        point = 0
+        for i in range(8):
+            for j in range(8):
+                if board[i][j] == 1:
+                      if i<=5 and 1 <= j <= 6:
+                          point += 15+i
+
+                if board[i][j] == -1:
+                      if i>=3 and 1 <= j <= 6:
+                          point-=(22+i)
+
+        return point
+    def king_dominance(self,board):
+        pass
+
 
 
     def evaluate(self, board):
         score = 0
         for row in board:
-            score+=sum(row)*10
+            score+=sum(row)*100
 
-        score+=len(m.legal_moves(1,board))
-        score-=len(m.legal_moves(-1,board))
+        score+=len(m.legal_moves(1,board))*5
+        score-=len(m.legal_moves(-1,board))*5
+        score+=self.centralization(board)
         return score
 
-    def game_over(self,board,player):
-        if m.legal_moves(player,board) == 0:
-            return False
 
 
 
